486f1f5ea6377cb7ea3403cd6048775195bf4be1
Edukacja <edukacja@Landingi-MacBook-Pro.local>
2023-02-04 17:44:43 +0100

    Break component added

---
+++ src/components/Break/Break.stories.tsx
@@ -0,0 +1,12 @@
+import { ComponentMeta, ComponentStory } from '@storybook/react'
+
+import { Break } from './Break'
+
+export default {
+	title: 'Break',
+	component: Break,
+} as ComponentMeta<typeof Break>
+
+const Template: ComponentStory<typeof Break> = args => <Break {...args} />
+
+export const Default = Template.bind({})
