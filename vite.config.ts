d992c172c539fa9086c0fe48ef898f758c922903
Edukacja <edukacja@Landingi-MacBook-Pro.local>
2023-02-04 17:06:14 +0100

    vite configured

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,24 @@
-import { defineConfig } from 'vite'
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
 import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+	plugins: [react()],
+	test: {
+		globals: true,
+		environment: 'jsdom',
+		setupFiles: ['./src/config/tests.tsx'],
+		coverage: {
+			all: true,
+			include: [
+				'src/components/**/*.{ts,tsx}',
+				'src/config/**/*.{ts,tsx}',
+				'src/constants/**/*.{ts,tsx}',
+				'src/utils/**/*.{ts,tsx}',
+			],
+			exclude: ['src/**/index.{ts,tsx}', 'src/**/*.test.{ts,tsx}'],
+			reporter: ['json-summary', 'text', 'html'],
+		},
+	},
 })
