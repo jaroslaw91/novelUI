7f3d1eba2d5e13b2f5e3cd046e822f59231f7b48
Edukacja <edukacja@Landingi-MacBook-Pro.local>
2023-02-05 12:29:23 +0100

    package config updated

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,37 @@
 /// <reference types="vitest" />
 /// <reference types="vite/client" />
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 import { defineConfig } from 'vite'
+import dts from 'vite-plugin-dts'
+import tsConfigPaths from 'vite-tsconfig-paths'
+
+import * as packageJson from './package.json'
 
 export default defineConfig({
-	plugins: [react()],
+	plugins: [
+		react(),
+		tsConfigPaths(),
+		dts({
+			include: ['src'],
+		}),
+	],
+	build: {
+		lib: {
+			entry: resolve('src', 'index.ts'),
+			name: 'SimpleUI',
+			formats: ['es', 'umd'],
+			fileName: format => `simple-ui.${format}.js`,
+		},
+		rollupOptions: {
+			external: [...Object.keys(packageJson.peerDependencies)],
+		},
+	},
 	test: {
 		globals: true,
 		environment: 'jsdom',
 		setupFiles: ['./src/config/tests.tsx'],
+
 		coverage: {
 			all: true,
 			include: [
