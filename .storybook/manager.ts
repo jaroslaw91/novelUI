eecf473491bbf980747726ee84b7a8c62c00c906
Edukacja <edukacja@Landingi-MacBook-Pro.local>
2023-02-05 14:24:45 +0100

    code fixed

--- .storybook/manager.ts
+++ .storybook/manager.ts
@@ -1,13 +1,7 @@
 import { addons } from '@storybook/addons'
 
 import novelUI from './novelUI'
-import favicon from './static/icon.png'
 
 addons.setConfig({
 	theme: novelUI,
 })
-
-const link = document.createElement('link')
-link.setAttribute('rel', 'shortcut icon')
-link.setAttribute('href', favicon)
-document.head.appendChild(link)
