1138ad8079a14209a452cd931f6d824029231e25
Edukacja <edukacja@Landingi-MacBook-Pro.local>
2023-02-19 22:54:32 +0100

    GlobalStyles updated

--- src/styles/GlobalStyles.ts
+++ src/styles/GlobalStyles.ts
@@ -1,26 +1,33 @@
 import { createGlobalStyle, css } from 'styled-components'
 
+import PoppinsMediumWoff from '../assets/fonts/PoppinsMedium.woff'
+import PoppinsMediumWoff2 from '../assets/fonts/PoppinsMedium.woff2'
+import PoppinsRegularWoff from '../assets/fonts/PoppinsRegular.woff'
+import PoppinsRegularWoff2 from '../assets/fonts/PoppinsRegular.woff2'
+import PoppinsSemiBoldWoff from '../assets/fonts/PoppinsSemiBold.woff'
+import PoppinsSemiBoldWoff2 from '../assets/fonts/PoppinsSemiBold.woff2'
+
 export const GlobalStyles = createGlobalStyle`
 	${({ theme: { font, colors } }) =>
 		css`
 		@font-face {
 			font-family: Poppins;
-			src: url('../assets/fonts/PoppinsRegular.woff2') format('woff2'),
-				url('../assets/fonts/PoppinsRegular.woff') format('woff');
+			src: url(${PoppinsRegularWoff2}) format('woff2'),
+				url(${PoppinsRegularWoff}) format('woff');
 			font-weight: 400;
 		}
 	
 		@font-face {
-			font-family: Roboto;
-			src: url('../assets/fonts/PoppinsMedium.woff2') format('woff2'),
-				url('../assets/fonts/PoppinsMedium.woff') format('woff');
+			font-family: Poppins;
+			src: url(${PoppinsMediumWoff2}) format('woff2'),
+				url(${PoppinsMediumWoff}) format('woff');
 			font-weight: 500;
 		}
 	
 		@font-face {
-			font-family: Roboto;
-			src: url('../assets/fonts/PoppinsSemiBold.woff2') format('woff2'),
-				url('../assets/fonts/PoppinsSemiBold.woff') format('woff');
+			font-family: Poppins;
+			src: url(${PoppinsSemiBoldWoff2}) format('woff2'),
+				url(${PoppinsSemiBoldWoff}) format('woff');
 			font-weight: 600;
 		}
 	
