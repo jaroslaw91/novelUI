ff983070b6e7f272659819afdf7760ce8cc0551c
Edukacja <edukacja@Landingi-MacBook-Pro.local>
2023-02-24 21:39:55 +0100

    modal fixed

---
+++ src/utils/modalShow.ts
@@ -0,0 +1,34 @@
+import NiceModal from '@ebay/nice-modal-react'
+import { ReactEventHandler, ReactNode } from 'react'
+
+import { Modal } from '../components/Modal'
+
+export type ModalShowProps = {
+	id: string
+	title: string
+	subtitle?: string
+	content: ReactNode
+	variant?: 'primary' | 'secondary' | 'alert'
+	action: ReactEventHandler<HTMLButtonElement>
+	actionName: string
+	cancelName: string
+}
+
+export const modalShow = ({
+	id,
+	title,
+	subtitle = '',
+	content,
+	actionName,
+	cancelName,
+	variant = 'primary',
+}: ModalShowProps) =>
+	NiceModal.show(Modal, {
+		id,
+		title,
+		subtitle,
+		content,
+		actionName,
+		cancelName,
+		variant,
+	})
