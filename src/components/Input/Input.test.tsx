3a49850fd338a85635177ec809c83a07cb7ecd1d
Edukacja <edukacja@Landingi-MacBook-Pro.local>
2023-02-26 17:55:26 +0100

    Input added

---
+++ src/components/Input/Input.test.tsx
@@ -0,0 +1,76 @@
+import { describe, fireEvent, it, render, vi, waitFor } from 'utils/tests-utils'
+
+import { Input } from './Input'
+
+const onChange = vi.fn()
+
+describe('components/Input', () => {
+	it('should have label, name & id', async () => {
+		const { getByTestId } = render(
+			<Input
+				label="label"
+				name="input"
+				id="input"
+				onChange={onChange}
+			/>,
+		)
+
+		await waitFor(() => {
+			expect(getByTestId('input')).toBeInTheDocument()
+			expect(getByTestId('input-label')).toBeInTheDocument()
+		})
+	})
+
+	it('should have errors', async () => {
+		const { getByTestId } = render(
+			<Input
+				label="label"
+				name="input"
+				id="input"
+				onChange={onChange}
+				errors={{ input: 'error' }}
+				touched={{ input: true }}
+			/>,
+		)
+
+		await waitFor(() => {
+			expect(getByTestId('input')).toBeInTheDocument()
+			expect(getByTestId('input-error')).toBeInTheDocument()
+		})
+	})
+
+	it('should have full width', async () => {
+		const { getByTestId } = render(
+			<Input
+				label="label"
+				name="input"
+				id="input"
+				onChange={onChange}
+				hasFullWidth
+			/>,
+		)
+
+		await waitFor(() => {
+			expect(getByTestId('input')).toBeInTheDocument()
+		})
+	})
+
+	it('should be password', async () => {
+		const { getByTestId } = render(
+			<Input
+				label="label"
+				name="input"
+				id="input"
+				onChange={onChange}
+				type="password"
+			/>,
+		)
+
+		await waitFor(() => {
+			expect(getByTestId('input')).toBeInTheDocument()
+			expect(getByTestId('input-password')).toBeInTheDocument()
+
+			fireEvent.click(getByTestId('input-password'))
+		})
+	})
+})
---
+++ src/components/Input/Input.tsx
@@ -0,0 +1,127 @@
+import { FormikErrors, FormikTouched } from 'formik'
+import { ChangeEventHandler, FC, FocusEventHandler, ReactEventHandler, useState } from 'react'
+import { useTranslation } from 'react-i18next'
+import { HiEye, HiEyeSlash } from 'react-icons/hi2'
+
+import { Paragraph } from '../Paragraph'
+import {
+	IconComponent,
+	InputComponent,
+	InputWrapperComponent,
+	LabelComponent,
+} from './Input.styles'
+import { PasswordMeter } from './PasswordMeter'
+
+export interface InputProps {
+	type?: 'text' | 'email' | 'password'
+	label?: string
+	name: string
+	id: string
+	value?: string
+	placeholder?: string
+	touched?: FormikTouched<{
+		[field: string]: boolean
+	}>
+	errors?: FormikErrors<{
+		[field: string]: string
+	}>
+	onChange?: ChangeEventHandler<HTMLInputElement>
+	onBlur?: FocusEventHandler<HTMLInputElement>
+	autoFocus?: boolean
+	hasFullWidth?: boolean
+	hasPasswordMeter?: boolean
+	isDropdown?: boolean
+	hideError?: boolean
+	onClick?: ReactEventHandler<HTMLInputElement>
+	'data-testid'?: string
+}
+
+export const Input: FC<InputProps> = ({
+	type = 'text',
+	label,
+	name,
+	placeholder = '',
+	id,
+	value = '',
+	touched = {},
+	errors = {},
+	onChange,
+	onBlur,
+	autoFocus = false,
+	hasFullWidth = false,
+	hasPasswordMeter = false,
+	isDropdown = false,
+	hideError = false,
+	onClick,
+	'data-testid': dataTestId = 'input',
+}) => {
+	const { t } = useTranslation()
+
+	const [showPassword, setShowPassword] = useState(false)
+
+	const isError = (errors[name] && touched[name]) as boolean
+	const isPassword = (type === 'password') as boolean
+
+	const handleShowPassword = () => setShowPassword(!showPassword)
+
+	return (
+		<div>
+			{label && (
+				<LabelComponent
+					htmlFor={id}
+					hasFullWidth={hasFullWidth}
+				>
+					<Paragraph
+						type="label"
+						data-testid="input-label"
+					>
+						{label}
+					</Paragraph>
+				</LabelComponent>
+			)}
+
+			<InputWrapperComponent>
+				<InputComponent
+					id={id}
+					name={name}
+					type={showPassword ? 'text' : type}
+					value={value}
+					placeholder={placeholder}
+					onChange={onChange}
+					onBlur={onBlur}
+					hasFullWidth={hasFullWidth}
+					autoFocus={autoFocus}
+					isError={isError}
+					isPassword={isPassword}
+					data-testid={dataTestId}
+					isDropdown={isDropdown}
+					onClick={onClick}
+				/>
+
+				{isPassword && (
+					<IconComponent
+						onClick={handleShowPassword}
+						data-testid="input-password"
+					>
+						{showPassword ? <HiEyeSlash /> : <HiEye />}
+					</IconComponent>
+				)}
+
+				{isPassword && !isError && hasPasswordMeter && <PasswordMeter value={value} />}
+			</InputWrapperComponent>
+
+			{!hideError && (
+				<Paragraph
+					type="error"
+					size="small"
+					align="left"
+					data-testid="input-error"
+				>
+					{isError ? t(`${errors[name]}`) : null}
+				</Paragraph>
+			)}
+		</div>
+	)
+}
+
+Input.displayName = 'Input'
