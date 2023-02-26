27cca713834e5b5474a12683d4c0ce50ee71b48e
Edukacja <edukacja@Landingi-MacBook-Pro.local>
2023-02-27 00:18:42 +0100

    Tooltip styled updated

--- src/components/Tooltip/Tooltip.styles.ts
+++ src/components/Tooltip/Tooltip.styles.ts
@@ -1,4 +1,3 @@
-import { Arrow } from 'react-laag'
 import styled, { css } from 'styled-components'
 
 export const TooltipComponent = styled.div`
@@ -10,16 +9,6 @@ export const TooltipComponent = styled.div`
 			color: ${colors.main};
 			padding: ${spaces.small} ${spaces.medium};
 			text-align: center;
-			z-index: 9999;
-		`}
-`
-
-export const ArrowComponent = styled(Arrow)`
-	${({ theme: { colors } }) =>
-		css`
-			background-color: ${colors.main};
-			color: ${colors.main};
-
 			z-index: 9999;
 		`}
 `
